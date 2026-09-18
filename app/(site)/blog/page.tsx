0e64ef17cc02c63d9eacc0b0b8a34857c253ce34
Golam Mostofa <mostofa.sundarbancourier@gmail.com>
2026-09-19 00:22:10 +0600

    Change blog title from 'From the Blog' to 'Blog Post'

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -36,7 +36,7 @@ export default async function BlogPage({
     <>
       <PageHero
         breadcrumb={[{ label: "Home", href: "/" }, { label: "Blog" }]}
-        title="From the Blog"
+        title="Blog Post"
         titleBn="ব্লগ"
         description="News, updates and guides on the Bangla media landscape."
       />
